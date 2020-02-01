802ee7779c6971d20cffc8f6dce6479e67a53d3e
Andre Gustavo Castro <agcastro.py@yahoo.com>
2020-02-01 20:57:28 -0300

    Added Product Detail Screen

--- lib/screens/product_detail.dart
+++ lib/screens/product_detail.dart
@@ -8,12 +8,48 @@ class ProductDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context).settings.arguments as String;
-    final loadedProduct = Provider.of<Products>(context, listen: false).findById(productId);
+    final loadedProduct =
+        Provider.of<Products>(context, listen: false).findById(productId);
 
     return Scaffold(
       appBar: AppBar(
         title: Text(loadedProduct.title),
       ),
-    );
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 250,
+              width: double.infinity,
+              child: Image.network(loadedProduct.imageUrl, fit: BoxFit.cover,),
+            ),
+            SizedBox(height: 10,),
+            Text(
+              '\$${loadedProduct.price}',
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 20
+              ),
+            ),
+            SizedBox(height: 10,),
+            Container(
+              width: double.infinity,
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: Text(
+                loadedProduct.title,
+                textAlign: TextAlign.center,
+                softWrap: true,
+                style: TextStyle(
+                  fontSize: 26,
+                  color: Theme.of(context).accentColor
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+
+      );
+
   }
 }
